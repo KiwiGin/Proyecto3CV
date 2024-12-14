af7c5a00f80b55cc81fc584818f0a688c14917ec
KiwiGin <bayeskiwigen@gmail.com>
2024-12-14 13:56:19 -0500

    Update image paths and adjust SGBM parameters for depth map computation

--- depth.map.py
+++ depth.map.py
@@ -6,8 +6,8 @@ import os
 class DepthMap:
     def __init__(self, showImages):
         root = os.getcwd()
-        imgLeftPath = os.path.join(root, 'demoImages//cubo//WIN_20241115_13_44_18_Pro.jpg')
-        imgRightPath = os.path.join(root, 'demoImages//cubo//WIN_20241115_13_44_25_Pro.jpg')
+        imgLeftPath = os.path.join(root, 'demoImages//cubo//TESTLEFT.jpg')
+        imgRightPath = os.path.join(root, 'demoImages//cubo//TESTRIGHT.jpg')
         self.imgLeft = cv.imread(imgLeftPath, cv.IMREAD_GRAYSCALE)
         self.imgRight = cv.imread(imgRightPath, cv.IMREAD_GRAYSCALE)
 
@@ -32,9 +32,9 @@ class DepthMap:
         plt.show()
 
     def computeDepthMapSGBM(self):
-        window_size = 5
-        min_disp = 0
-        nDispFactor = 1  # Número de disparidades a considerar
+        window_size = 7
+        min_disp = 16
+        nDispFactor = 14  # Número de disparidades a considerar
         num_disp = 16 * nDispFactor - min_disp
         num_disp = (num_disp // 16) * 16  # Asegurarse de que sea múltiplo de 16
 
@@ -43,10 +43,10 @@ class DepthMap:
             numDisparities=num_disp,
             blockSize=window_size,
             P1=8 * 3 * window_size**2,
-            P2=50 * 3 * window_size**2,
+            P2=32 * 3 * window_size**2,
             disp12MaxDiff=1,
-            uniquenessRatio=60,
-            speckleWindowSize=20,
+            uniquenessRatio=15,
+            speckleWindowSize=0,
             speckleRange=2,
             preFilterCap=63,
             mode=cv.STEREO_SGBM_MODE_SGBM_3WAY
